e1169d979a39472f5699b25d0f94a85465330780
Prasad7901 <mndprasad1999@gmail.com>
2024-07-10 13:51:24 +0530

    adding rules of shell

---
+++ 00-shell.sh
@@ -0,0 +1,11 @@
+#shell script
+**steps:**
+
+*create repo in github
+*clone repo in local laptop
+*start developing
+*add the files to temp area
+*commit the changes
+*push the changes to github/central repo
+
+&naming convention:use small cases with hyphens
